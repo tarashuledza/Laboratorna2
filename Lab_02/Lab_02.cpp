41b236fb5592a9db7d1e6ccae2a6cb9e77ee20da
Taras <tarashuledza14@gmail.com>
2022-10-06 22:39:41 +0300

    branch 1 version 1

---
+++ Lab_02/Lab_02.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double a;
+	double z1;
+	double z2;
+
+	cout << "a ="; cin >> a;
+	z1 = cos(6 * a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+	z2 = 4 * cos(a / 2) * cos(5 / 2) * cos(4 * a);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
