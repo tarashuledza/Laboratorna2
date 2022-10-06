4d39874015f654a2f0a4e8d549983332c819f2ba
Taras <tarashuledza14@gmail.com>
2022-10-06 22:47:26 +0300

    branch 2 version 1

--- Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02.cpp
@@ -10,12 +10,12 @@ int main()
 	double z2;
 
 	cout << "a ="; cin >> a;
-	//z1 = cos(6 * a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
-	z2 = 4 * cos(a / 2) * cos(5 / 2) * cos(4 * a);
+	z1 = cos(6 * a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+	//z2 = 4 * cos(a / 2) * cos(5 / 2) * cos(4 * a);
 
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
